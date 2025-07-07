e75e2149d8beb3c511d3219fe4bedf3fb0c44f33
Joyal-George-KJ <joyalgeorgekj@gmail.com>
2025-07-07 20:54:04 +0530

    Fix: Freezing after first render when trigger change added: forceRerender();

--- src/demo/TextAnimations/FallingTextDemo.jsx
+++ src/demo/TextAnimations/FallingTextDemo.jsx
@@ -120,6 +120,7 @@ const FallingTextDemo = () => {
             width={150}
             onChange={(val) => {
               setTrigger(val);
+              forceRerender();
             }}
           />
 
