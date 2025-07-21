f0670101adc82df6f0c61129252e8a81505157f9
David Haz <davidhzdev@gmail.com>
2025-07-21 20:15:42 +0300

    Fix syntax error in StickerPeel usage example

--- src/constants/code/Animations/stickerPeelCode.js
+++ src/constants/code/Animations/stickerPeelCode.js
@@ -20,7 +20,7 @@ import logo from './assets/sticker.png'
   peelBackActivePct={40}
   shadowIntensity={0.6}
   lightingIntensity={0.1}
-  initialPosition={{ x: -100, y: 100 }}}
+  initialPosition={{ x: -100, y: 100 }}
 />`,
   code,
   css,
